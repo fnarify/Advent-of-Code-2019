2c7a0e2a00009355199ed59721cf7e6d0dbf17cb
fnarify <16514203+fnarify@users.noreply.github.com>
2020-01-05 22:21:53 +1100

    Day 18 progress

--- 18/aoc18.c
+++ 18/aoc18.c
@@ -8,7 +8,7 @@
 #define MID (DIM / 2)
 #define NKEY 26
 
-enum Tile      {Wall = '#', Move = '.'};
+enum Tile      {Key = 0, Door = 1, Wall = '#', Move = '.'};
 enum Direction {North = 1, South = 2, West = 3, East = 4};
 
 const int vec[][2] = {
@@ -28,14 +28,6 @@ struct Queue {
     int front, rear, size;
 };
 typedef struct Queue Queue;
-
-int  qempty   (Queue *);
-void enq      (Queue *, int);
-int  deq      (Queue *);
-int  iskey    (char);
-int  isdoor   (char);
-void printdist(int *, Point *, char, int);
-int  search   (char [DIM][DIM + 3], int, int, int, Queue *, int *);
 
 int qempty(Queue *q)
 {
@@ -64,40 +56,67 @@ int deq(Queue *q)
     return q->val[q->front++];
 }
 
-int iskey(char c)
+int istype(char c)
 {
-    return isalpha(c) && islower(c);
+    return isalpha(c) ? islower(c) ? Key : Door : c;
 }
 
-int isdoor(char c)
+int contain(char c, int keys)
 {
-    return isalpha(c) && isupper(c);
+    return keys && (1 << (tolower(c) - 'a'));
 }
 
-void printdist(int *dist, Point *loc, char orig, int w)
+void addkey(char c, int *keys)
 {
-    int i, cur;
-    printf("\nstarting from key %c:\n", orig);
-    for (i = 'a'; i < 'z'; i++)
-    {
-        cur = loc[i].y * w + loc[i].x;
-        if (i == orig || !dist[cur]) {continue;}
-        printf("dist from %c to %c: %d\n", orig, i, dist[cur]);
-    }
+    *keys |= 1 << (tolower(c) - 'a');
+}
+
+int allkeyfnd(int keys)
+{
+    int i, n;
+    for (i = 0, n = 0; i < NKEY; i++) {n += (1 << i);} // 67108863
+    return keys == n;
 }
 
 /**
- * maybe should be a depth-first search as it would let me properly add key/door pairs
- * currently does a breadth-first search from the point x, y of a maze w x w in size
- * OR just stop the search when you hit a door that you don't have a key for
- *
- * returns how many keys it can reach
+ * prints the distances from orig to all reachable keys
+ * also calculates the key that is the shortest distance away that hasn't already
+ * been found and returns it's name
  */
-int search(char maze[DIM][DIM + 3], int x, int y, int w, Queue *que, int *dist)
+char showdist(int *dist, int w, Point *loc, char orig, int keys, int print)
 {
-    int i, cur, cx, cy, next, nx, ny, pos, kfound;
+    char minchar = 'A';
+    int i, index, minval = INT_MAX;
+    printf("\nstarting from key %c:\n", orig);
+    for (i = 'a'; i <= 'z'; i++)
+    {
+        index = loc[i].y * w + loc[i].x;
+        if (i == orig || !dist[index]) {continue;}
+        if (dist[index] < minval && !contain(i, keys))
+        {
+            minval = dist[index];
+            minchar = i;
+        }
+        if (print) {printf("dist from %c to %c: %d\n", orig, i, dist[index]);}
+    }
+    if (print)
+    {
+        if (minchar != 'A') {printf(" -smallest dist to %c is %d\n", minchar, minval);}
+        else                {printf(" -no keys can be moved to\n");}
+    }
+    return minchar;
+}
 
-    kfound = 0;
+/**
+ * breadth-first search from the point x, y of a maze w x w in size
+ * the search will not go past a key if it detects one, or a door if the key for that door
+ * in keys is not found, thus it will only record immediately reachable points
+ * keys are stored in a bitmap as there are only 26 maximum keys
+ */
+void search(char maze[DIM][DIM + 3], int x, int y, int w, Queue *que, int *dist, int keys)
+{
+    int i, cur, cx, cy, next, nx, ny, pos;
+
     que->front = 0;
     que->rear = -1;
     enq(que, x);
@@ -117,23 +136,24 @@ int search(char maze[DIM][DIM + 3], int x, int y, int w, Queue *que, int *dist)
             next = ny * w + nx;
             pos = maze[ny][nx];
 
-            if ((pos == Move || iskey(pos)) && !dist[next]
-                && cx && cy && cx < w && cy < w)
+            // don't need bounds checks as they're all walls
+            if (!dist[next] && istype(pos) != Wall)
             {
-                enq(que, nx);
-                enq(que, ny);
                 dist[next] = dist[cur] + 1;
-                if (iskey(pos)) {kfound++;}
+                if (pos == Move || (istype(pos) == Door && contain(pos, keys)))
+                {
+                    enq(que, nx);
+                    enq(que, ny);
+                }
             }
         }
     }
-    return kfound;
 }
 
 int main()
 {
     const int lsize = 'z' + 1;
-    int i, j, msize;
+    int i, j, msize, fndkey = 0, min, cmin;
     char maze[DIM][DIM + 3];
     int **dist;
     Queue *que;
@@ -142,8 +162,7 @@ int main()
     i = 0;
     while (fgets(maze[i++], sizeof(maze[0]), stdin)) {;}
 
-    // record key and door locations
-    // keys are lowercase, doors are uppercase
+    // record key and door locations -- keys are lowercase, doors are uppercase
     for (i = 0; i < DIM; i++)
     {
         for (j = 0; j < DIM; j++)
@@ -171,13 +190,15 @@ int main()
     que->val = malloc(sizeof(*que->val) * que->size);
     if (!que->val) {return -3;}
 
-    // fill dist array with distance from each key (and origin) to every other key
-    search(maze, MID, MID, DIM, que, dist[NKEY]);
-    printdist(dist[NKEY], loc, '@', DIM);
-    for (i = 'a'; i < 'z'; i++)
+    // fill dist array with distance from each key (and origin) to every other reachable key
+    // i.e, we ignore keys that are blocked by doors or other keys
+    min = cmin = INT_MAX;
+    search(maze, MID, MID, DIM, que, dist[NKEY], fndkey);
+    showdist(dist[NKEY], DIM, loc, '@', fndkey, 1);
+    for (i = 'a'; i <= 'z'; i++)
     {
-        search(maze, loc[i].x, loc[i].y, DIM, que, dist[i - 'a']);
-        printdist(dist[i - 'a'], loc, i, DIM);
+        search(maze, loc[i].x, loc[i].y, DIM, que, dist[i - 'a'], fndkey);
+        showdist(dist[i - 'a'], DIM, loc, i, fndkey, 1);
     }
 
     free(que->val);
